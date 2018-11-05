a6c4fc6ded2a59635e13f96f1984f077bc1da70b
abhishek-luthra <abhishek.l@cashify.in>
2018-11-05 20:27:00 +0530

    data binding added

--- app/src/main/java/com/chargingwatts/chargingalarm/ui/batteryprofile/BatteryProfileFragment.kt
+++ app/src/main/java/com/chargingwatts/chargingalarm/ui/batteryprofile/BatteryProfileFragment.kt
@@ -8,12 +8,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ProgressBar
-import android.widget.TextView
 import androidx.work.PeriodicWorkRequest
 import com.chargingwatts.chargingalarm.BaseFragment
 import com.chargingwatts.chargingalarm.R
+import com.chargingwatts.chargingalarm.databinding.FragmentBatteryProfileBinding
+import com.chargingwatts.chargingalarm.ui.vibrate.VibrationManager
+import com.chargingwatts.chargingalarm.util.autoCleared
 import com.chargingwatts.chargingalarm.util.battery.BATTERY_WORKER_REQUEST_TAG
 import com.chargingwatts.chargingalarm.util.battery.BatteryChangeReciever
 import com.chargingwatts.chargingalarm.util.battery.BatteryMonitoringService
@@ -28,60 +28,74 @@ class BatteryProfileFragment : BaseFragment() {
     lateinit var periodicBatteryUpdater: PeriodicBatteryUpdater
     @Inject
     lateinit var batteryChangeReciever: BatteryChangeReciever
+
     private lateinit var batteryProfileViewModel: BatteryProfileViewModel
 
-    private lateinit var mBtnStart: Button
-    private lateinit var mBtnStop: Button
-    private lateinit var mTvConnectionStatus: TextView
-    private lateinit var mTvBatteryLevel: TextView
-    private lateinit var mPbBatteryLevel: ProgressBar
+    var binding by autoCleared<FragmentBatteryProfileBinding>()
+
+    var mediaPlayer: MediaPlayer? = null
+
+    val callback = object : View.OnClickListener {
+        override fun onClick(view: View?) {
+            when (view?.id) {
+                R.id.btn_start_service -> {
+                    context?.let { lContext ->
+                        mediaPlayer?.start()
+                        VibrationManager.init(lContext)
+                        VibrationManager.makePattern().beat(2000).rest(1000).playPattern(60)
+                        batteryProfileViewModel.setUserAlarmPreference(true)
+                        BatteryMonitoringService.startInForeground(lContext)
+                    }
+                }
+
+                R.id.btn_stop_service -> {
+                    context?.let { lContext ->
+                        mediaPlayer?.stop()
+                        mediaPlayer?.prepare()
+                        VibrationManager.stop()
+                        batteryProfileViewModel.setUserAlarmPreference(false)
+                        BatteryMonitoringService.stopService(lContext)
+                    }
+                }
+            }
+        }
+
+    }
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_battery_profile, container, false)
-        mBtnStart = view.findViewById(R.id.btn_start_battery_update)
-        mBtnStop = view.findViewById(R.id.btn_stop_battery_update)
-        mTvConnectionStatus = view.findViewById(R.id.tv_power_connection_status)
-        mTvBatteryLevel = view.findViewById(R.id.tv_battery_level)
-        mPbBatteryLevel = view.findViewById(R.id.pb_battery_level)
-        val mediaPlayer = MediaPlayer.create(context,R.raw.ultra_alarm)
-        mediaPlayer.isLooping = true
 
-        mediaPlayer.setOnCompletionListener { MediaPlayer.OnCompletionListener{
-            mediaPlayer.stop()
-//            mediaPlayer.reset()
-//            mediaPlayer.release()
-        } }
+        batteryProfileViewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(BatteryProfileViewModel::class.java)
+        binding = FragmentBatteryProfileBinding.inflate(inflater)
+        binding.setLifecycleOwner(this)
+        val lBatteryProfileLiveData = batteryProfileViewModel.batteryProfileLiveData
+        lBatteryProfileLiveData.observe(this, Observer { lBatteryProfile ->
+            binding.batteryProfile = lBatteryProfile
+        })
+        binding.clickListener = callback
 
-        mBtnStart.setOnClickListener {
-            context?.let { lContext ->
-//                mediaPlayer.start()
-//                VibrationManager.init(lContext)
-//                VibrationManager.makePattern().beat(2000).rest(1000).playPattern(60)
-                batteryProfileViewModel.setUserAlarmPreference(true)
-                BatteryMonitoringService.startInForeground(lContext)
+
+        mediaPlayer = MediaPlayer.create(context, R.raw.ultra_alarm)
+        mediaPlayer?.isLooping = true
+
+        mediaPlayer?.setOnCompletionListener {
+            MediaPlayer.OnCompletionListener {
+                mediaPlayer?.stop()
             }
         }
-        mBtnStop.setOnClickListener {
-            context?.let { lContext ->
-//                mediaPlayer.stop()
-//                mediaPlayer.prepare()
-//                VibrationManager.stop()
-                batteryProfileViewModel.setUserAlarmPreference(false)
-                BatteryMonitoringService.stopService(lContext)
-            }
-        }
-        return view
 
+        return binding.root
     }
 
     override fun onResume() {
         super.onResume()
         periodicBatteryUpdater.startPeriodicBatteryUpdate(PeriodicWorkRequest.MIN_PERIODIC_INTERVAL_MILLIS, BATTERY_WORKER_REQUEST_TAG)
-        context?.let{
+        context?.let {
             batteryChangeReciever.registerReciever(it)
         }
-     }
+    }
 
     override fun onPause() {
         super.onPause()
@@ -91,22 +105,4 @@ class BatteryProfileFragment : BaseFragment() {
         }
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        batteryProfileViewModel = ViewModelProviders.of(this, viewModelFactory)
-                .get(BatteryProfileViewModel::class.java)
-
-        batteryProfileViewModel.batteryProfileLiveData.observe(this, Observer { batteryProfile ->
-            batteryProfile?.apply {
-                remainingPercent?.let {
-                    mTvBatteryLevel.text = "${it}%"
-                    mPbBatteryLevel.progress = it
-                }
-            }
-        })
-
-        batteryProfileViewModel.isChargingPreference.observe(this, Observer {
-            mTvConnectionStatus.text = it.toString()
-        })
-    }
 }
